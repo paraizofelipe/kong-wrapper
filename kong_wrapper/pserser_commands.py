da5a236d03cb2875dfe6ff36894c089728ea1d1e
felipeparaizo <felipeparaizo@gmail.com>
2017-10-19 02:06:05 -0200

    Abilitado execução de ações via YAML

--- kong_wrapper/pserser_commands.py
+++ kong_wrapper/pserser_commands.py
@@ -1,12 +1,20 @@
 import yaml
 import argparse
+import simplejson as json
+from kong_wrapper.utils import read_conf
+from pygments import highlight, lexers, formatters
 
 
 class Command:
 
     def __init__(self, file_path):
         self.info_file = self.parser_file(file_path)
-        print(self.info_file)
+        # print(self.info_file)
+
+    @staticmethod
+    def show_json(json):
+        colorful_json = highlight(json.encode('UTF-8'), lexers.JsonLexer(), formatters.TerminalFormatter())
+        print(colorful_json)
 
     @staticmethod
     def parser_file(file_path):
@@ -31,21 +39,51 @@ class Command:
             parser.add_argument(arg['name'], help=arg['help'])
 
         for command in self.info_file['commands']:
+            object_actions_ = command['actions'].__class__()
             aux_parser = subparser.add_parser(command['name'], help=command['help'])
             aux_sub_parser = aux_parser.add_subparsers(help='Commands')
 
             for sub_command in command['subcommands']:
                 aux_sub_command = aux_sub_parser.add_parser(sub_command['name'], help=sub_command['help'])
 
-                for arg in sub_command['arguments']:
-                    if 'define' in arg:
-                        args_defines = self.extract_defines(arg['define'], self.info_file['define'])
-                        for arg_define in args_defines:
-                            aux_sub_command.add_argument(arg_define['name'], **arg_define['params'])
-                    else:
-                        aux_sub_command.add_argument(arg['name'], **arg['params'])
+                if {"arguments"}.issubset(sub_command):
+                    for arg in sub_command['arguments']:
+                        if 'define' in arg:
+                            args_defines = self.extract_defines(arg['define'], self.info_file['define'])
+                            for arg_define in args_defines:
+                                aux_sub_command.add_argument(arg_define['name'], **arg_define['params'])
+                        else:
+                            aux_sub_command.add_argument(arg['name'], **arg['params'])
+
+                if {"exec"}.issubset(sub_command):
+                    func_exec = getattr(object_actions_, sub_command['exec'])
+                else:
+                    func_exec = getattr(object_actions_, sub_command['name'])
+                aux_sub_command.set_defaults(func=func_exec)
 
         args = parser.parse_args()
+
+        try:
+            cli_conf = read_conf('default')
+            if not args.host or not args.port:
+                if cli_conf and cli_conf["host"] and cli_conf["port"]:
+                    args.host = cli_conf["host"]
+                    args.port = cli_conf["port"]
+
+                    try:
+                        result_request = args.func(args)
+                        result_request = json.dumps(result_request, sort_keys=True, indent=True)
+                        # print(result_request)
+                        self.show_json(result_request)
+                    except Exception as error:
+                        raise error
+
+                else:
+                    raise argparse.ArgumentError(subparser, "host and port values have not been defined.")
+        except FileNotFoundError:
+            print('The file ".kong" not found in directory "home" of user!')
+        except Exception as error:
+            raise error
 
 
 if __name__ == "__main__":
